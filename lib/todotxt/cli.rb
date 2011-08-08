8e6d42cf9a40ef3bc2ae6cd5504e75ee6664d0f1
Johan Sahlén <johan.sahlen@gmail.com>
2011-08-08 17:53:21 +0200

    Begin implementing config file

--- lib/todotxt/cli.rb
+++ lib/todotxt/cli.rb
@@ -1,16 +1,27 @@
 require "thor"
 require "rainbow"
 
+require "pp"
+
 module Todotxt
+  CFG_PATH = File.expand_path("~/.todotxt.fg")
+
   class CLI < Thor
     include Thor::Actions
     include Todotxt::CLIHelpers
 
-    @@list = TodoList.new
+    def initialize(*args)
+      super
+
+      parse_config
+
+      @list = TodoList.new
+    end
 
     desc "list | ls [SEARCH]", "List all todos, or todos matching SEARCH"
     def list search=""
-      @@list.filter(search)
+      puts @hello
+      @list.filter(search)
 
       render_list
     end
@@ -19,7 +30,7 @@ module Todotxt
     desc "add | a TEXT", "Add a new Todo item"
     def add(str, *str2)
       str = "#{str} #{str2.join(' ')}"
-      todo = @@list.add str
+      todo = @list.add str
 
       puts format_todo(todo)
     end
@@ -27,15 +38,27 @@ module Todotxt
 
     desc "list_projects | lsp", "List all projects"
     def list_projects
-      @@list.projects.each { |p| say p.color(:green) }
+      @list.projects.each { |p| say p.color(:green) }
     end
     map "lsp" => :list_projects
 
     desc "list_contexts | lsc", "List all contexts"
     def list_contexts
-      @@list.contexts.each { |c| say c.color(:cyan) }
+      @list.contexts.each { |c| say c.color(:cyan) }
     end
     map "lsc" => :list_contexts
+
+    desc "generate_config", "Create a .todotxt.cfg file in your home folder, containing the path to todo.txt"
+    def generate_config
+      say "You need a .todotxt.cfg file in your home folder to continue (used to determine the path of your todo.txt.) Answer yes to have it generated for you (pointing to ~/todo.txt), or no to create it yourself."
+      confirm_generate = yes? "=> ".color(:green) + "Create ~/.todotxt.cfg? [y/N]"
+
+      if confirm_generate
+        puts "yes"
+      else
+        puts "no"
+      end
+    end
 
     desc "version", "Show todotxt version"
     def version
@@ -45,11 +68,18 @@ module Todotxt
     private
 
     def render_list
-      numsize = @@list.count + 1
+      numsize = @list.count + 1
       numsize = numsize.to_s.length + 0
 
-      @@list.each do |t|
+      @list.each do |t|
         say format_todo(t, numsize)
+      end
+    end
+
+    def parse_config
+      unless File.exist? CFG_PATH
+        generate_config
+        exit
       end
     end
   end
