fc40e5753e66e70f8646065579635cf959573b16
Bèr Kessels <ber@webschuur.com>
2012-11-30 01:11:43 +0100

    Raise error when both old and new config are used

--- lib/todotxt/config.rb
+++ lib/todotxt/config.rb
@@ -12,6 +12,8 @@ module Todotxt
       copy_new unless File.exists? @config_path
 
       super @config_path
+
+      validate
     end
 
     def files
@@ -19,6 +21,12 @@ module Todotxt
     end
 
     private
+    def validate
+      if params["files"] && params["todo_txt_path"]
+        raise "Bad configuration file: use either files or todo_txt_path"
+      end
+    end
+
     def copy_new
       FileUtils.copy "conf/todotxt.cfg", @config_path
     end
