64778bd9613ebf2c2c3b793884c0df1b75632a0f
Bèr Kessels <ber@webschuur.com>
2012-12-13 21:13:41 +0100

    Move auto-generate out of init.

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -5,11 +5,6 @@ describe Todotxt::Config do
     ENV["HOME"] = File.join "/", "tmp"
     @config_file = File.join ENV["HOME"], ".todotxt.cfg"
   end
-  it 'should create a template on initialize' do
-    File.delete @config_file if File.exists? @config_file
-    Todotxt::Config.new
-    File.exists?(@config_file).should be_true
-  end
 
   it 'should have a "files"' do
     cfg = Todotxt::Config.new "spec/fixtures/config_new.cfg"
@@ -25,5 +20,14 @@ describe Todotxt::Config do
     expect do
       Todotxt::Config.new "spec/fixtures/config_both.cfg"
     end.to raise_error "Bad configuration file: use either files or todo_txt_path"
+  end
+
+  context "#generate!" do
+    it 'should create a template' do
+      File.delete @config_file if File.exists? @config_file
+      todo = Todotxt::Config.new
+      todo.generate!
+      File.exists?(@config_file).should be_true
+    end
   end
 end
