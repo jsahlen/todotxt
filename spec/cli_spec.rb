a495256b1303d44029e10c700b6fa8695dd49468
Bèr Kessels <ber@webschuur.com>
2013-01-05 22:21:24 +0100

    removing cli testing. Will be replaced with Aruba soon.

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -2,14 +2,4 @@ require "todotxt/clihelpers"
 require "todotxt/cli"
 
 describe Todotxt::CLI do
-  it 'should have a move command' do
-    Todotxt::CLI.tasks.should have_key "move"
-  end
-
-  context "move" do
-    before(:each) do
-      @task = Todotxt::CLI.tasks["move"]
-      @cli  = Todotxt::CLI.new
-    end
-  end
 end
