f1d2918361fba261199518114e505cfe3f6847ee
Bèr Kessels <ber@webschuur.com>
2013-01-23 16:14:34 +0100

    Create example todo.txt on initialisation

--- lib/todotxt/todofile.rb
+++ lib/todotxt/todofile.rb
@@ -1,16 +1,20 @@
 module Todotxt
-  class TodoFile < File
+  class TodoFile
     #From Kernel::File
     #@arg filename, String, path to the file
     #@arg mode, String, IO-mode, defaults to a+
-    #@arg offer_template, Boolean, Whether or not to offer creation of file from template.
-    def initialize *args
-      # Default to append-mode
-      if args[2].nil?
-        args[1] = "a+"
-      end
+    #@arg generate, Boolean, Force generation of a todofile from template
+    def initialize path
+      @path = path
+    end
 
-      super *args
+    # Generate a file from template
+    def generate!
+      FileUtils.copy File.join(File.dirname(File.expand_path(__FILE__)), "..", "..", "conf", "todo.txt"), @path
+    end
+
+    def exists?
+      File.exists? File.expand_path(@path)
     end
 
     def self.from_key(key)
@@ -21,6 +25,11 @@ module Todotxt
       else
         raise "Key not found in config"
       end
+    end
+
+
+    def to_s
+      @path
     end
   end
 end
