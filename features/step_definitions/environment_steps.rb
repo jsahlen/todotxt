1ebbb3e32c3f1b3900294766ad716c5ebacf2e98
Bèr Kessels <ber@webschuur.com>
2013-02-04 16:33:16 +0100

    Feature to move items between files

--- features/step_definitions/environment_steps.rb
+++ features/step_definitions/environment_steps.rb
@@ -2,6 +2,12 @@ Given /^a default config exists$/ do
   write_file(".todotxt.cfg", "[files]\ntodo = todo.txt")
 end
 
+Given /^a config exists with the following files:$/ do |files|
+  files_directive = "[files]\n"
+  files_directive += files.hashes.map {|file| "#{file["alias"]}=#{file["path"]}" }.join("\n")
+  write_file(".todotxt.cfg", files_directive)
+end
+
 Given /^a todofile exists$/ do
   write_file("todo.txt", "Read documentation for todotxt\nWrite cucumber steps for todotxt")
 end
@@ -14,3 +20,8 @@ Given /^a todofile with the following items exists:$/ do |todolist|
   contents = todolist.hashes.map {|row| row["todo"] }.join("\n")
   write_file("todo.txt", contents)
 end
+
+Given /^an empty todofile named "(.*?)" exists$/ do |filename|
+  write_file(filename, "")
+end
+
