288095266c912304b4a982c09cf4c631e1630c42
Bèr Kessels <ber@webschuur.com>
2013-03-14 15:04:16 +0100

    introduce before_date filter

--- spec/todolist_spec.rb
+++ spec/todolist_spec.rb
@@ -60,6 +60,13 @@ describe Todotxt::TodoList do
       @list.on_date(date).should eql [@list.todos.last]
     end
 
+    it "fetchs items before a cereain date" do
+      @list.add "2012-11-11 item"
+      @list.add "2012-12-12 item"
+      date = DateTime.parse("2012-12-12")
+      @list.before_date(date).count.should eql 1
+    end
+
     it "includes done items in search when told to do so" do
       @list.filter "first", :with_done => true
 
